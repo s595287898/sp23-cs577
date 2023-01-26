9359b66e4885924b1f231e03af54f7aa0ca736a3
s595287898 <s595287898@163.com>
2023-01-25 21:51:07 -0600

    tested and sumbmited

--- homework1/homework1.java
+++ homework1/homework1.java
@@ -1,15 +1,16 @@
-package homework1;
+//package homework1;
 import java.util.*;
 public class homework1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
+        input.nextLine();
         String[] res = new String[num];
         for(int i=0;i<num;i++){
-            res[i]=input.next();
+            res[i]=input.nextLine();
         }
         for (String string : res) {
-            System.out.println("Hello, "+string);
+            System.out.println("Hello, "+string+"!");
         }
     }
    
